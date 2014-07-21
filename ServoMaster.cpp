c8bd67dbbf8fe040dc115a5f64e74b8f739e24a1
Konstantin Gredeskoul <kig@wanelo.com>
2014-07-20 22:01:14 -0700

    Refactoring and renaming for readability

--- ServoMaster.cpp
+++ ServoMaster.cpp
@@ -26,9 +26,10 @@ void ServoMaster::detach() {
 	_right.detach();
 }
 
+
 void ServoMaster::moveAtCurrentSpeed() {
-	_left.writeMicroseconds(speedToMicroseconds(_currentSpeedPercent));
-	_right.writeMicroseconds(speedToMicroseconds(-_currentSpeedPercent));
+	_left.writeMicroseconds(convertSpeedPercentToMicroseconds(_currentSpeedPercent));
+	_right.writeMicroseconds(convertSpeedPercentToMicroseconds(-_currentSpeedPercent));
 }
 
 void ServoMaster::goForward(unsigned short speedPercent) {
@@ -60,15 +61,9 @@ void ServoMaster::goBackward(unsigned short speedPercent,
 void ServoMaster::turn(int angle, maneuverCallback callback) {
 	stop();
 	signed short speed = 100 * (angle == 0 ? 1 : abs(angle) / angle);
-	_left.writeMicroseconds(speedToMicroseconds(speed));
-	_right.writeMicroseconds(speedToMicroseconds(speed));
+	_left.writeMicroseconds(convertSpeedPercentToMicroseconds(speed));
+	_right.writeMicroseconds(convertSpeedPercentToMicroseconds(speed));
 	startManeuverTimer(abs(angle) * 8, callback);
-}
-
-void ServoMaster::stopManeuverTimer() {
-	_maneuver.running = false;
-	_maneuver.durationMs = 0;
-	_maneuver.startMs = 0;
 }
 
 void ServoMaster::stop() {
@@ -81,6 +76,22 @@ bool ServoMaster::isMoving() {
 	return (_currentSpeedPercent != 0);
 }
 
+
+bool ServoMaster::isManeuvering() {
+	checkManeuveringState();
+	return _maneuver.running;
+}
+
+//________________________________________________________________________________________
+//
+// Private
+
+void ServoMaster::stopManeuverTimer() {
+	_maneuver.running = false;
+	_maneuver.durationMs = 0;
+	_maneuver.startMs = 0;
+}
+
 void ServoMaster::checkManeuveringState() {
 	if (_maneuver.running && millis() - _maneuver.startMs > _maneuver.durationMs) {
 		stop();
@@ -88,16 +99,11 @@ void ServoMaster::checkManeuveringState() {
 		maneuverCallback oldCallback = _maneuver.callback;
 		_maneuver.callback = NULL;
 
-		// this "trick" allows us to call the callback which might
+		// call the callback (if defined) using previously saved poiner
 		// start another maneuver, and yet reset _maneuverCallback
 		// if no new maneuver was started.
 		if (oldCallback != NULL) oldCallback();
 	}
-}
-
-bool ServoMaster::isManeuvering() {
-	checkManeuveringState();
-	return _maneuver.running;
 }
 
 void ServoMaster::startManeuverTimer(unsigned int durationMs,
@@ -108,16 +114,14 @@ void ServoMaster::startManeuverTimer(unsigned int durationMs,
 	_maneuver.running = true;
 }
 
-
-
 // speed can be between -100 and +100
-int ServoMaster::speedToMicroseconds(int speedPercent) {
-	int value = (int) (1500 + // that's the middle
-			200.0 * // plus/minus 200
-					atan((double) (1.55 * speedPercent / 100.0))); // this makes curve more linear
-//	Serial.print("Converted Speed ");
-//	Serial.print(speedPercent);
-//	Serial.print(" to Microseconds ");
-//	Serial.println(value);
+int ServoMaster::convertSpeedPercentToMicroseconds(int speedPercent) {
+	int value = (int) (
+			// 1500 +
+			(SERVO_MIN_MS + SERVO_HALF_RANGE_MS) +
+			// +/- 200
+			SERVO_HALF_RANGE_MS *
+			// this ranges -1..1 for speedPercent between 0 and 100
+			atan((double) (1.55 * speedPercent / 100.0)));
 	return value;
 }
--- ServoMaster.h
+++ ServoMaster.h
@@ -10,10 +10,10 @@
 #ifndef ServoMaster_H
 #define ServoMaster_H
 
-#if ARDUINO < 100
-#include <WProgram.h>
+#if defined(ARDUINO) && ARDUINO >= 100
+	#include <Arduino.h>
 #else
-#include <Arduino.h>
+	#include <WProgram.h>
 #endif
 
 typedef void(*maneuverCallback)(void);
@@ -24,6 +24,20 @@ typedef struct maneuverStruct {
 	maneuverCallback callback;
 	bool running;
 } maneuver;
+
+#ifndef SERVO_MIN_MS
+#define SERVO_MIN_MS 1300
+#endif
+
+#ifndef SERVO_MAX_MS
+#define SERVO_MAX_MS 1700
+#endif
+
+// this would be 200 for the above values.
+#ifndef SERVO_HALF_RANGE_MS
+#define SERVO_HALF_RANGE_MS (SERVO_MAX_MS - SERVO_MIN_MS / 2)
+#endif
+
 
 #include <Servo.h>
 
@@ -54,7 +68,7 @@ private:
 	Servo _left;
 	Servo _right;
 
-	int speedToMicroseconds(int speedPercent);
+	int convertSpeedPercentToMicroseconds(int speedPercent);
 	void moveAtCurrentSpeed();
 	void checkManeuveringState();
 	void startManeuverTimer(unsigned int durationMs, maneuverCallback callback);
